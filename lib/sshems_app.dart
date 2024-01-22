dd230ba99848d9aa3d815f69eb5a949008f77c2b
ENIOLA SODIQ OLADIPUPO <sodeeqe03@gmail.com>
2024-01-22 13:06:38 +0100

    updated UI

--- lib/sshems_app.dart
+++ lib/sshems_app.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 import 'dart:math';
 
@@ -14,44 +16,76 @@ class SshemsApp extends StatefulWidget {
 
 class _SshemsAppState extends State<SshemsApp> {
   double voltage = 230.0;
+  double batteryChargeCurrent = 4.5,
+      batterydisChargeCurrent = 3.7,
+      bateryVoltage = 54.5;
   List<DragAndDropList> _contents = [];
-  late Stream<double> voltageStream;
+  late Stream<List<double>> voltageStream;
+  List<double> valsList =
+      []; // AC volatge, DC chargeCur, DC dischargeCur, DC voltage
   bool voltageSupply = true;
   List tiersName = ["Lights", "Power Outlets", "Power Appliances", "Others"];
+  List tiersCaption = [
+    "All lights in the house",
+    "Sockets and all power outlets",
+    "All power appliances in the house",
+    "Other devices on the last line",
+  ];
 
   @override
   void initState() {
     super.initState();
 
-    voltageStream = Stream<double>.periodic(
+    voltageStream = Stream<List<double>>.periodic(
       const Duration(seconds: 1),
-      (count) => Random().nextInt(5) + 230.0,
+      (count) => [
+        Random().nextInt(5) + 230.0,
+        Random().nextInt(3) / 3 + 4.5,
+        Random().nextInt(3) / 3 + 3.7,
+        Random().nextInt(4) / 3.9 + 54.6,
+      ],
     );
 
-    voltageStream.listen((v) {
+    voltageStream.listen((vals) {
       setState(() {
-        voltage = v;
+        voltage = vals[0];
+        batteryChargeCurrent = vals[1];
+        batterydisChargeCurrent = vals[2];
+        bateryVoltage = vals[3];
       });
     });
 
     _contents = List.generate(4, (index) {
       return DragAndDropList(
         horizontalAlignment: MainAxisAlignment.center,
+        // verticalAlignment: CrossAxisAlignment.center,
         decoration: BoxDecoration(
-            color: Colors.blue.withOpacity(0.3),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(width: 3, color: Colors.white)),
-        header: Text(
-          '\n   Tier ${index + 1}',
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
+          color: Colors.blue.withOpacity(0.9),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            width: 3,
+            color: Colors.white,
+          ),
+        ),
+        header: Padding(
+          padding: const EdgeInsets.only(top: 12, bottom: 2),
+          child: Text(
+            "  ${tiersName[index]}",
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
         children: [
           DragAndDropItem(
-            child: Text("   ${tiersName[index]}"),
+            child: Text(
+              "   ${tiersCaption[index]}",
+              style: const TextStyle(
+                color: Colors.white60,
+              ),
+            ),
             canDrag: false,
           ),
         ],
@@ -87,117 +121,54 @@ class _SshemsAppState extends State<SshemsApp> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        voltageSupply = !voltageSupply;
-                      });
-                    },
-                    child: Container(
-                      width: size.width * 0.43,
-                      height: size.width * 0.55,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 184, 217, 245),
-                        borderRadius: BorderRadius.circular(26),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 35,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CircleAvatar(
-                            radius: 25,
-                            backgroundColor: Colors.blue.shade200,
-                            child: const Center(
-                              child: Icon(
-                                CupertinoIcons.bolt,
-                                color: Colors.red,
-                                size: 30,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 12),
-                          voltageSupply == true
-                              ? Text(
-                                  "Voltage\nsupplied:",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.grey.shade700),
-                                )
-                              : Text(
-                                  "No Voltage\nsupplied:",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.grey.shade700),
-                                ),
-                          voltageSupply == true
-                              ? Text(
-                                  '${voltage.toStringAsFixed(2)} V',
-                                  style: const TextStyle(
-                                      fontSize: 26,
-                                      fontWeight: FontWeight.w500),
-                                )
-                              : const Text(
-                                  '0.00 V',
-                                  style: TextStyle(
-                                      color: Colors.redAccent,
-                                      fontSize: 26,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                        ],
-                      ),
-                    ),
+                  _detailsCard(
+                    title: "AC Voltage\nsupplied:",
+                    value: '${voltage.toStringAsFixed(2)} V',
+                    bgColor: const Color.fromARGB(255, 184, 217, 245),
+                    iconData: CupertinoIcons.bolt,
+                    iconColor: Colors.blue,
+                    iconBgColor: Colors.white,
                   ),
-                  Container(
-                    width: size.width * 0.43,
-                    height: size.width * 0.55,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 191, 241, 230),
-                      borderRadius: BorderRadius.circular(26),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 35,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const CircleAvatar(
-                          radius: 25,
-                          backgroundColor: Color.fromARGB(255, 226, 238, 226),
-                          child: Center(
-                            child: Icon(
-                              Icons.electric_meter,
-                              color: Colors.green,
-                              size: 30,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          "Active power usage:",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.grey.shade700),
-                        ),
-                        Text(
-                          voltageSupply == true
-                              ? '${(voltage * 9 / 1000).toStringAsFixed(2)} kW'
-                              : "0.124 kW",
-                          style: const TextStyle(
-                              fontSize: 26, fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
+                  _detailsCard(
+                    title: "Active power\nusage:",
+                    value: '${(voltage * 5.7 / 1000).toStringAsFixed(2)} kW',
+                    bgColor: const Color.fromARGB(255, 191, 241, 230),
+                    iconData: Icons.electric_meter,
+                    iconColor: Colors.teal,
+                    iconBgColor: Colors.white,
                   ),
                 ],
+              ),
+              SizedBox(height: size.width * 0.03),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _detailsCard(
+                    title: "Battery charging\ncurrent:",
+                    value: '${batteryChargeCurrent.toStringAsFixed(2)} A',
+                    bgColor: const Color.fromARGB(255, 241, 216, 138),
+                    iconData: Icons.battery_charging_full_outlined,
+                    iconColor: Colors.amber.shade800,
+                    iconBgColor: Colors.white,
+                  ),
+                  _detailsCard(
+                    title: "Battery discharging\ncurrent:",
+                    value: '${batterydisChargeCurrent.toStringAsFixed(2)} A',
+                    bgColor: const Color.fromARGB(255, 250, 196, 192),
+                    iconData: CupertinoIcons.battery_25,
+                    iconColor: Colors.red.shade700,
+                    iconBgColor: const Color.fromARGB(255, 226, 238, 226),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.width * 0.03),
+              _detailsCard(
+                title: "Battery\nvoltage:",
+                value: '${bateryVoltage.toStringAsFixed(2)} V',
+                bgColor: const Color.fromARGB(255, 199, 247, 200),
+                iconData: CupertinoIcons.battery_full,
+                iconColor: Colors.green,
+                iconBgColor: const Color.fromARGB(255, 226, 238, 226),
               ),
               const SizedBox(height: 30),
               const Row(
@@ -215,7 +186,6 @@ class _SshemsAppState extends State<SshemsApp> {
               const SizedBox(height: 14),
               SizedBox(
                 height: 400,
-                // color: Colors.amber,
                 child: DragAndDropLists(
                   children: _contents,
                   itemDragOnLongPress: true,
@@ -227,6 +197,63 @@ class _SshemsAppState extends State<SshemsApp> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _detailsCard({
+    required String title,
+    required String value,
+    required Color bgColor,
+    required IconData iconData,
+    required Color iconColor,
+    required Color iconBgColor,
+  }) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width * 0.43,
+      height: size.width * 0.55,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(26),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 35,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: iconBgColor,
+            child: Center(
+              child: Icon(
+                iconData,
+                color: iconColor,
+                size: 30,
+              ),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey.shade700,
+            ),
+          ),
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
       ),
     );
   }
